086e1a8cac0498b2da799c680ecd7e4a71cce1f2
NilsDem <nils.demerle@gmail.com>
2025-03-20 18:53:04 +0100

    fix scripts again

--- after_scripts/main_cli.py
+++ after_scripts/main_cli.py
@@ -26,27 +26,27 @@ def main():
     command = sys.argv[1]
 
     if command == 'train':
-        from scripts import train
+        from after_scripts import train
         sys.argv[0] = train.__name__
         app.run(train.main)
     elif command == 'prepare_dataset':
-        from scripts import prepare_dataset
+        from after_scripts import prepare_dataset
         sys.argv[0] = prepare_dataset.__name__
         app.run(prepare_dataset.main)
     elif command == 'update_dataset':
-        from scripts import update_dataset
+        from after_scripts import update_dataset
         sys.argv[0] = update_dataset.__name__
         app.run(update_dataset.main)
     elif command == 'train_autoencoder':
-        from scripts import train_autoencoder
+        from after_scripts import train_autoencoder
         sys.argv[0] = train_autoencoder.__name__
         app.run(train_autoencoder.main)
     elif command == 'export_autoencoder':
-        from scripts import export_autoencoder
+        from after_scripts import export_autoencoder
         sys.argv[0] = export_autoencoder.__name__
         app.run(export_autoencoder.main)
     elif command == 'export':
-        from scripts import export
+        from after_scripts import export
         sys.argv[0] = export.__name__
         app.run(export.main)
     else:
